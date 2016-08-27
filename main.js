2693414e4d731cd8bb22e6e6b52313ce6c793ffc
Joel Macey <macey003@gmail.com>
2016-08-27 17:20:30 +1000

    style changes

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-WebSocketServer = require('websocket').server;
-var http = require('http');
+/*function connect_to_websocket() {
+    document.write('start as well');
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-var server = http.createServer(function(request, response) {
-    // process HTTP request. Since we're writing just WebSockets server
-    // we don't have to implement anything.
-});
-server.listen(1337, function() { });
+    var connection = new WebSocket('ws://10.0.73.141:1337');
 
-// create the server
-wsServer = new WebSocketServer({
-    httpServer: server
-});
+    connection.onopen = function () {
+        // connection is opened and ready to use
+        document.write('ok then');
+    };
 
-console.log("starting");
+    connection.onerror = function (error) {
+        // an error occurred when sending/receiving data
+        document.write('error, man!!!');
+        document.write(error);
+    };
 
-// WebSocket server
-wsServer.on('request', function(request) {
-	console.log("request function");
-    var connection = request.accept(null, request.origin);
-
-    // This is the most important callback for us, we'll handle
-    // all messages from users here.
-    connection.on('message', function(message) {
-        console.log("hello");
-    });
-
-    connection.on('close', function(connection) {
-        // close user connection
-    });
-});
-
+    connection.onmessage = function (message) {
+        // try to decode json (I assume that each message from server is json)
+        try {
+            document.write(message.data);
+            //var json = JSON.parse(message.data);
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+        // handle incoming message
+    };
+};
+connect_to_websocket(); */
