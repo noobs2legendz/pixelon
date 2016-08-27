26154f3690f42c88ecfd8f6ee1c8b514f379cb48
Joel Macey <macey003@gmail.com>
2016-08-27 15:49:35 +1000

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    // process HTTP request. Since we're writing just WebSockets server
+    // we don't have to implement anything.
+});
+server.listen(1337, function() { });
+
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+console.log("starting");
+
+// WebSocket server
+wsServer.on('request', function(request) {
+	console.log("request function");
+    var connection = request.accept(null, request.origin);
+
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        console.log("hello");
+    });
+
+    connection.on('close', function(connection) {
+        // close user connection
+    });
+});
+
